af11592d2151b09082083eb5e6ce0986e2b89ae6
Alexis Langlet <alexis.langlet@etu.umontpellier.fr>
2023-06-14 15:42:28 +0200

    feat: AMQP use Main as the entrypoint

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -1,10 +1,43 @@
 import zio._
 
+$if(add_message_queue.truthy)$
+import nl.vroste.zio.amqp.*
+import nl.vroste.zio.amqp.model.*
+import message_queue.*
+import java.net.URI
+$endif$
+
 object Main extends ZIOAppDefault {
+
+  $if(add_message_queue.truthy)$
+
+  val channel: ZIO[Scope, Throwable, Channel] = for {
+    host <- ZIO.succeed(sys.env.getOrElse("RABBITMQ_HOST", "localhost"))
+    port <- ZIO.succeed(sys.env.getOrElse("RABBITMQ_PORT", "5672").toInt)
+    user <- ZIO.succeed(sys.env.getOrElse("RABBITMQ_USER", "guest"))
+    password <- ZIO.succeed(sys.env.getOrElse("RABBITMQ_PASSWORD", "guest"))
+    uri <- ZIO.succeed(URI.create(s"amqp://\$user:\$password@\$host:\$port"))
+    connection <- Amqp.connect(uri)
+    channel <- Amqp.createChannel(connection)
+  } yield channel
+
+  $endif$
+
   override def run =
     for {
+
       $if(add_http_server.truthy) $
-      _ <- HttpServer.run
+      h <- HttpServer.run.fork
+      $endif$
+      $if(add_message_queue.truthy) $
+      // Run a message queue example
+      channel <- channel // Create a channel
+      p <- ExampleProducer.produce(channel).fork // Start a producer that will send a message every second
+      c <- ExampleListener.listen(channel).fork // Start a consumer that will print the messages it receives
+      _ <- p.zip(c).join
+      $endif$
+      $if(add_http_server.truthy) $
+      _ <- h.join
       $endif$
     } yield 0
 }
