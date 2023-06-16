90cc801b9ed9787d2e461d3ff439131d6d09b12d
Alexis-Bernard <alexisbernard34310@gmail.com>
2023-06-16 15:15:12 +0200

    docs: add doc for http server

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,7 +1,6 @@
 export default {
   logo: <span>SMS Documentation</span>,
   project: {
-    link: 'https://github.com/do4-2022/scala-microservice-seed.g8',
-  }
-}
-
+    link: "https://github.com/do4-2022/scala-microservice-scaffold.g8",
+  },
+};
